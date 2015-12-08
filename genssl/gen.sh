189d40dde0db58e974803b6af01dd6fae8dbc2e0
Aaron Schlesinger <arschles@gmail.com>
2015-12-07 17:02:02 -0800

    fix(gen.sh,manifest_replace.sh): use .key instead of .pem extension
    Heroku’s instructions for creating a self-signed cert are very clear
    and easy to follow. using ‘.key’ to follow those instructions more
    closely

--- genssl/gen.sh
+++ genssl/gen.sh
@@ -4,8 +4,10 @@
 apk add --update-cache openssl
 rm -rf /var/cache/apk/*
 
+# these commands are adapted from the very clear and extensive Heroku documents on creating a self-signed SSL certificate: https://devcenter.heroku.com/articles/ssl-certificate-self#generate-private-key-and-certificate-signing-request
+
 openssl genrsa -des3 -passout pass:x -out ./genssl/server.pass.key 2048
-openssl rsa -passin pass:x -in ./genssl/server.pass.key -out ./genssl/server.pem
+openssl rsa -passin pass:x -in ./genssl/server.pass.key -out ./genssl/server.key
 rm ./genssl/server.pass.key
 openssl req -new -key ./genssl/server.key -subj "/C=US/ST=California/L=San Francisco/O=Engine Yard" -out ./genssl/server.csr
 # generate the cert
