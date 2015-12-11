f053b3d19e62c56070a145ad988395ac2972a474
Aaron Schlesinger <arschles@gmail.com>
2015-12-11 14:43:29 -0800

    feat(deploy.sh): deploying to quay & docker hub regardless of branch

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,7 +5,15 @@
 
 cd "$(dirname "$0")" || exit 1
 
-export IMAGE_PREFIX=deisci VERSION=v2-alpha
+BRANCH=$(git symbolic-ref --short -q HEAD)
+
+VER=v2-alpha
+if ![ $BRANCH == "master" ]; then
+  # deploy to "test-$BRANCH" tag if on a branch
+  VER="test-$BRANCH"
+fi
+
+export IMAGE_PREFIX=deisci VERSION=$VER
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 DEIS_REGISTRY='' make -C .. build docker-build docker-push
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
